8cefd0a89df5d8db21e687f31f3334041a64d304
vandersonbatista27 <vanderbezerra27@gmail.com>
2026-03-06 16:23:16 -0300

    fix: estilização da tela de login e cadastro

---
+++ src/pages/login.js
@@ -0,0 +1,53 @@
+import { useState } from "react"
+import { signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "../services/firebase"
+import { useRouter } from "next/router"
+import styles from "../styles/login.module.css"
+
+export default function Login() {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const router = useRouter()
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password)
+      router.push("/dashboard")
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+
+      <form className={styles.form} onSubmit={handleLogin}>
+
+        <h2 className={styles.title}>Login</h2>
+
+        <input
+          className={styles.input}
+          type="email"
+          placeholder="Digite seu email"
+          onChange={(e)=>setEmail(e.target.value)}
+        />
+
+        <input
+          className={styles.input}
+          type="password"
+          placeholder="Digite sua senha"
+          onChange={(e)=>setPassword(e.target.value)}
+        />
+
+        <button className={styles.button}>
+          Entrar
+        </button>
+
+      </form>
+
+    </div>
+  )
+}
